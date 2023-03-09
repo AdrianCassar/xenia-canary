96c9cbcf858b1939b856f9b28b2bb71fc4c65a24
Codie Stella <git@codie.gg>
2023-03-09 22:10:25 +0000

    Netplay proof-of-concept.

--- src/xenia/xbox.h
+++ src/xenia/xbox.h
@@ -693,7 +693,7 @@ struct X_XAMACCOUNTINFO {
                              AccountReservedFlags::kPasswordProtected);
   }
 
-  bool IsLiveEnabled() {
+  bool IsLiveEnabled() const {
     return static_cast<bool>(reserved_flags &
                              AccountReservedFlags::kLiveEnabled);
   }
