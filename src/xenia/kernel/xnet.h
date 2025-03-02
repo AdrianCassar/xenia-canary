185b94f163203bb8defa6d6aaefc513fe200fb81
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-03-02 15:53:30 +0000

    [XLiveBase] Implemented XStorageDelete

--- src/xenia/kernel/xnet.h
+++ src/xenia/kernel/xnet.h
@@ -43,6 +43,7 @@ namespace xe {
 #define X_ONLINE_E_SESSION_FULL                             static_cast<X_HRESULT>(0x80155202L)
 #define X_ONLINE_E_STORAGE_INVALID_FACILITY                 static_cast<X_HRESULT>(0x8015C009L)
 #define X_ONLINE_E_STORAGE_FILE_NOT_FOUND                   static_cast<X_HRESULT>(0x8015C004L)
+#define X_ONLINE_E_STORAGE_INVALID_STORAGE_PATH             static_cast<X_HRESULT>(0x8015C008L)
 
 #define X_PARTY_E_NOT_IN_PARTY                              static_cast<X_HRESULT>(0x807D0003L)
 
@@ -311,6 +312,22 @@ struct X_INVITE_INFO {
 };
 
 #pragma pack(pop)
+
+struct Internal_Marshalled_Data {
+  uint8_t unkn1_data[22];
+  xe::be<uint32_t> start_args_ptr;  // CArgumentList*
+  uint8_t unkn2_data[14];
+  xe::be<uint32_t> results_ptr;  // STRUCT*
+  xe::be<uint32_t> results_size;
+};
+
+struct XStorageDelete_Marshalled_Data {
+  xe::be<uint32_t> internal_data_ptr;
+  uint8_t unkn1_data[44];
+  xe::be<uint32_t> unkn1_ptr;
+  uint8_t unkn2_data[24];
+  xe::be<uint32_t> serialized_server_path_ptr;  // Entry 1
+};
 
 struct X_DATA_58024 {
   X_ARGUEMENT_ENTRY xuid;
