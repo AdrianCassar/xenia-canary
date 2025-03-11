5eafc91c36a802d1845e9d577a1bc17eb65b2af5
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-03-11 20:56:26 +0000

    [XLiveBase] Improved Blades dashboard marketplace stubs

--- src/xenia/kernel/xam/apps/xlivebase_app.h
+++ src/xenia/kernel/xam/apps/xlivebase_app.h
@@ -57,8 +57,20 @@ class XLiveBaseApp : public App {
   X_STORAGE_FACILITY GetStorageFacilityTypeFromServerPath(
       std::string server_path);
 
-  X_HRESULT Unk58024(uint32_t buffer_length);
-  X_HRESULT Unk5801C(uint32_t buffer_length);
+  X_HRESULT GenericMarshalled(uint32_t buffer_ptr);
+
+  X_HRESULT Unkn5008C(uint32_t buffer_ptr);
+  X_HRESULT XAccountGetPointsBalance(uint32_t buffer_ptr);
+  X_HRESULT Unkn5008B(uint32_t buffer_ptr);
+  X_HRESULT Unkn5008F(uint32_t buffer_ptr);
+  X_HRESULT Unkn50090(uint32_t buffer_ptr);
+  X_HRESULT Unkn50091(uint32_t buffer_ptr);
+  X_HRESULT Unkn50097(uint32_t buffer_ptr);
+
+  X_HRESULT Unkn5800A(uint32_t buffer_ptr);
+
+  X_HRESULT Unkn58024(uint32_t buffer_length);
+  X_HRESULT Unkn5801C(uint32_t buffer_length);
 
   static constexpr std::string_view xstorage_symboliclink = "XSTORAGE:";
 };
