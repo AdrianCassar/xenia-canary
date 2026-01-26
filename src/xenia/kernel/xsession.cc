e333b347acbc2aaf63a22cd721a5a745cd85aa15
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2026-01-26 22:30:24 +0000

    [NET] Implemented persistent console MAC address
    Fixed loading Halo 3 campaign save.

--- src/xenia/kernel/xsession.cc
+++ src/xenia/kernel/xsession.cc
@@ -1207,7 +1207,7 @@ void XSession::GetXnAddrFromSessionObject(SessionObjectJSON* session,
 
   const MacAddress mac = MacAddress(session->MacAddress());
 
-  memcpy(&XnAddr_ptr->abEnet, mac.raw(), sizeof(MacAddress));
+  memcpy(XnAddr_ptr->abEnet, mac.raw(), MacAddress::MacAddressSize);
 
   XnAddr_ptr->wPortOnline = session->Port();
 
