6852ac1f4a2074eff3a587218f526488af5b200e
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2026-04-16 20:41:22 +0100

    [XAM] Include platform.h before curl.h
    Fixed not defining NOMINMAX before including curl.h causing potential conflicts with MIN and MAX macros.

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -10,7 +10,6 @@
 #include "xenia/app/emulator_window.h"
 
 #include "third_party/imgui/imgui.h"
-#include "third_party/libcurl/include/curl/curl.h"
 #include "third_party/stb/stb_image_write.h"
 #include "third_party/tomlplusplus/toml.hpp"
 #include "xenia/base/assert.h"
